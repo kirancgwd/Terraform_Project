61cfabbfdb2594fef16613a1a42d6c5f180cda64
Kiran C <115028535+kirancgwd@users.noreply.github.com>
2025-08-25 17:19:45 +0530

    Update main.tf

--- EKS_Cluster/sg/main.tf
+++ EKS_Cluster/sg/main.tf
@@ -1,1 +1,36 @@
+resource "aws_security_group" "cluster_sg" {
+  vpc_id = var.vpc_id
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "node_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-node-sg"
+  }
+}
