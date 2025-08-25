3daf68e9435061304b9ae50ed1616551f4d6512b
Kiran C <115028535+kirancgwd@users.noreply.github.com>
2025-08-25 17:20:15 +0530

    Create variable.tf

---
+++ EKS_Cluster/sg/variable.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix for the security group names"
+  type        = string
+}
