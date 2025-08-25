739c0cac9b761427cd4558b86e32805f5bb805df
Kiran C <115028535+kirancgwd@users.noreply.github.com>
2025-08-25 17:16:06 +0530

    Update main.tf

--- EKS_Cluster/eks/main.tf
+++ EKS_Cluster/eks/main.tf
@@ -1,1 +1,46 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
 
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  count                   = length(var.availability_zones)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-route-table"
+  }
+}
+
+resource "aws_route_table_association" "association" {
+  count          = length(aws_subnet.subnet)
+  subnet_id      = aws_subnet.subnet[count.index].id
+  route_table_id = aws_route_table.route_table.id
+}
