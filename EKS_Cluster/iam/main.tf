ae6249c3daea2327a2d00dd22f7d81737742a31f
Kiran C <115028535+kirancgwd@users.noreply.github.com>
2025-08-25 18:18:54 +0530

    Update main.tf

--- EKS_Cluster/iam/main.tf
+++ EKS_Cluster/iam/main.tf
@@ -1,31 +1,14 @@
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "eks_cluster_role" {
-  name = var.cluster_role_name
-
-  assume_role_policy = var.cluster_assume_role_policy
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role" "eks_node_group_role" {
-  name = var.node_group_role_name
-
-  assume_role_policy = var.node_group_assume_role_policy
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_policy" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_cni_policy" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_group_registry_policy" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
