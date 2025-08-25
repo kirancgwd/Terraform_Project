2d57d11c3c0168f4d961876630d3777bda6c05f6
Kiran C <115028535+kirancgwd@users.noreply.github.com>
2025-08-25 19:02:11 +0530

    Update variable.tf

--- EKS_Cluster/eks/variable.tf
+++ EKS_Cluster/eks/variable.tf
@@ -1,5 +1,24 @@
-variable "name" {}
-variable "cluster_name" {}
-variable "cluster_version" {}
-variable "subnet_ids" { type = list(string) }
-variable "vpc_id" {}
+variable "name" {
+  description = "Environment or label name for tagging"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for EKS worker nodes"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC for the EKS cluster"
+  type        = string
+}
