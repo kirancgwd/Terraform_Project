44c6f497c1da3cc8910ccbdd14cebac13fb93759
Kiran C <115028535+kirancgwd@users.noreply.github.com>
2025-08-25 19:00:55 +0530

    Update variable.tf

--- EKS_Cluster/vpc/variable.tf
+++ EKS_Cluster/vpc/variable.tf
@@ -1,4 +1,19 @@
-variable "name" {}
-variable "vpc_cidr" {}
-variable "public_subnet_cidrs" { type = list(string) }
-variable "azs" { type = list(string) }
+variable "name" {
+  description = "Name to be used in resource naming and tags"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
